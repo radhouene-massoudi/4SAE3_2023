1cfe57af5390eaa01af88bf3b9a1fef3ab0b4c83
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-11-06 14:57:50 +0100

    seance2

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,10 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
-klass="4SAE3";
-kolor='red'
-
-showMsg(){
-  console.log("bonjour ");
-}
+  klass = "4SAE3";
+  kolor = 'red';
+  search='';
+  listResidences = [
+    {
+      "id": 1, "name": "El fel", "address": "Borj Cedria",
+      "image": "../../assets/images/R1.jpeg"
+    },
+    {
+      "id": 2, "name": "El yasmine",
+      "address": "Ezzahra", "image": "../../assets/images/R2.jpg"
+    },
+    {
+      "id": 3, "name": "El Arij",
+      "address": "Borj Cedria", "image": "../../assets/images/R3.jpg"
+    },
+    {
+      "id": 4, "name": "El Anber", "address": "Manzah 5",
+      "image": "../../assets/images/R4.jpg"
+    }
+  ];
+  showMsg() {
+    console.log("bonjour ");
+  }
+  changeColor(c: any) {
+    this.kolor = c
+  }
 }
